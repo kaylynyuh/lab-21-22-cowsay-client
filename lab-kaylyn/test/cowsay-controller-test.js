357733f832a866f39c6ffac0c1ac0df5a0ecdd78
kaylynyuh <yuhkay4@gmail.com>
2016-10-19 13:47:22 -0700

    karma-jasmine added, karma now working. 2 tests passing

--- lab-kaylyn/test/cowsay-controller-test.js
+++ lab-kaylyn/test/cowsay-controller-test.js
@@ -17,26 +17,27 @@ describe('testing cowsayCtrl', function(){
     });
   });
 
+
+
   describe('testing initial properties', () => {
     it('title should equal Hellarz', () => {
       expect(this.cowsayCtrl.title).toBe('Hellarz');
     });
 
     it('history should be empty array', () => {
-      expect(Array.isArray(this.cowsayCtrl.history)).toBe(true);
+      expect(Array.isArray(this.cowsayCtrl.historyArray)).toBe(true);
     });
 
     it('cowfile should be same as cowsay.list', () => {
       cowsay.list((err, list) => {
         expect(this.cowsayCtrl.files).toEqual(list);
         expect(this.cowsayCtrl.currentCow).toEqual(list[0]);
-
       });
     });
   });
 
   //test method that modifies scope correctly
-  describe('testing #updateCow()', () => {
+  describe('testing #update()', () => {
     it('should return a beavis.zen hello', () => {
       //get mock result
       //should return EXACTLY what updateCow is expecting, even with the newline
@@ -46,7 +47,17 @@ describe('testing cowsayCtrl', function(){
     });
   });
 
-  describe('testing #repeatCow()', () => {
+  describe('testing #duplicate()', () => {
+    it('should return a beavis.zen hello', () => {
+      //get mock result
+      //should return EXACTLY what updateCow is expecting, even with the newline
+      let expectedResult = '\n' + cowsay.say({text: 'hello', f: this.cowsayCtrl.currentCow});
+      let result = this.cowsayCtrl.updateCow('hello');
+      expect(result).toEqual(expectedResult);
+    });
+  });
+
+  describe('testing #undo()', () => {
     it('should return a beavis.zen hello', () => {
       //get mock result
       //should return EXACTLY what updateCow is expecting, even with the newline

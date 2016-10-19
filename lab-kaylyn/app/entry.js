1626e6b200734d395d255dabd1a8621aa54eb39f
kaylynyuh <yuhkay4@gmail.com>
2016-10-18 17:49:48 -0700

    debugging, reset cow stopped working

--- lab-kaylyn/app/entry.js
+++ lab-kaylyn/app/entry.js
@@ -13,48 +13,44 @@ const angular = require('angular');
 const demoApp = angular.module('demoApp', []); //create demoApp, this line is creative an angular module, it's a setter
 
 //angular-constructs
-demoApp.controller('CowsayController', ['$log', '$scope', CowsayController]);
+demoApp.controller('CowsayController', ['$log', CowsayController]);
 //creating a controller on the demoApp module ^
 
 //controller constructor function
 //$scope will create a new child scope
 //args have to be exact same order as dependency strings
-function CowsayController($log, $scope){
+function CowsayController($log){
   $log.debug('init CowsayController');
-  let cowsayCtrl = $scope.cowsayCtrl = {};
-  //attach properties to the $scope object. (via cowsayCtrl)
-  cowsayCtrl.title = 'Hellarz';
-  cowsayCtrl.show = false;
-  cowsayCtrl.state = null;
-  cowsayCtrl.stateArray = [];
-
-  cowsayCtrl.updateCow = function(input){
-    $log.debug('cowsayCtrl.updateCow()');
-    return '\n' + cowsay.say({text: input || 'Give me some s*** to say'});
-  };
-
-  cowsayCtrl.repeatCow = function(input){
-    $log.debug('cowsayCtrl.repeatCow()');
-    cowsayCtrl.stateArray.push(input);
-    cowsayCtrl.state = '\n' + cowsay.say({text: input});
-
-    if(cowsayCtrl.show === false) cowsayCtrl.show = true;
-  };
-
-
-  //attach a resetCow () to cowsayCtrl that checks the length of state array, if it's 0, don't show else...
-  cowsayCtrl.resetCow = function(){
-    $log.debug('cowsayCtrl.resetCow()');
-    cowsayCtrl.stateArray === 0 ? cowsayCtrl.show = false : cowsayCtrl.show = true;
-    cowsayCtrl.state = cowsay.say({text: cowsayCtrl.stateArray.pop()});
-    //invoke this method in the html to data bind to the template
-  };
+  this.title = 'Hellarz';
+  this.show = false;
+  this.state = null;
+  this.stateArray = [];
 
   cowsay.list((err, cowfiles) => {
     this.cowfiles = cowfiles;
     this.currentCow = this.cowfiles[0];
     console.log('this.cowfiles', this.cowfiles);
   });
+
+  this.updateCow = function(input){
+    $log.debug('this.updateCow()');
+    return '\n' + cowsay.say({text: input || 'Give me some s*** to say'});
+  };
+
+  this.repeatCow = function(input){
+    $log.debug('this.repeatCow()');
+    this.stateArray.push(input);
+    this.state = '\n' + cowsay.say({text: input});
+    if(this.show === false) this.show = true;
+  };
+
+  //attach a resetCow () to cowsayCtrl that checks the length of state array, if it's 0, don't show else...
+  this.resetCow = function(){
+    $log.debug('this.resetCow()');
+    this.stateArray === 0 ? this.show = false : this.show = true;
+    this.state = this.stateArray.pop() || '';
+    //invoke this method in the html to data bind to the template
+  };
 }
 
 demoApp.controller('NavController', ['$log', NavController]);
